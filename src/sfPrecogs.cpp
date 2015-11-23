5c6361ca10d5dd7620564dcdfb9629c12796d99c
martin <martincura@gmail.com>
2015-11-23 14:28:16 -0300

    Commit inicial.

---
+++ src/sfPrecogs.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : sfPrecogs.cpp
+// Author      : Precogs
+// Version     :
+// Copyright   : Robar es malo.
+// Description : TP Organización de Datos (75.06)
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Saludos
+	cout << "MAAIAAAMEEEEE" << endl;
+
+	// lectura de train
+	// Training
+
+	// lectura de test
+	// Predictions
+
+	// Escritura
+
+	// gracias totales
+	return 0;
+}
